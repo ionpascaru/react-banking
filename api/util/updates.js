a34241c5cd7a0e89b1a0cbd432fe6f59aa96fb53
ionpascaru <iionpascaru@gmail.com>
2020-02-20 13:20:31 +0100

    add utilities to handle data updates

---
+++ api/util/updates.js
@@ -0,0 +1,15 @@
+const { throwError, passError, handleValidationErrors } = require("./errors");
+
+exports.checkUpdatesValid = (req = {}, allowedUpdates = []) => {
+  const updates = Object.keys(req.body);
+  const isValidOperation = updates.every(update =>
+    allowedUpdates.includes(update)
+  );
+  if (!isValidOperation) {
+    throwError("Invalid updates", 400);
+  }
+  return;
+};
+exports.applyUpdates = (req, to = {}) => {
+  Object.keys(req.body).forEach(update => (to[update] = req.body[update]));
+};
