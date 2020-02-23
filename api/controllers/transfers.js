0bbab5f33c32cfd72f51b2bedd8181d80366eab0
ionpascaru <iionpascaru@gmail.com>
2020-02-23 13:29:25 +0100

    transfers controller

---
+++ api/controllers/transfers.js
@@ -0,0 +1,61 @@
+const Transfer = require("../models/transfer");
+
+// Utils
+const {
+  throwError,
+  passError,
+  handleValidationErrors
+} = require("../util/errors");
+const { checkUpdatesValid, applyUpdates } = require("../util/updates");
+
+// Get all transfers
+exports.getMyTransfers = async (req, res, next) => {
+  try {
+    let transfers = await Transfer.find({
+      $or: [{ sender: req.user._id }, { recipient: req.user._id }]
+    }).lean();
+
+    if (!transfers) {
+      throwError("No transfers found", 422);
+    }
+
+    res.status(200).json(transfers);
+  } catch (err) {
+    passError(err, next);
+  }
+};
+
+// Get one transfer
+exports.getSingle = async (req, res, next) => {
+  try {
+    let transfer = await Transfer.findOne({
+      _id: req.params.id,
+      $or: [{ sender: req.user._id }, { recipient: req.user._id }]
+    }).lean();
+
+    if (!transfer) {
+      throwError("No transfer found", 422);
+    }
+
+    res.status(200).json(transfer);
+  } catch (err) {
+    passError(err, next);
+  }
+};
+
+// Create new transfer
+exports.create = async (req, res, next) => {
+  const transfer = new Transfer(req.body);
+
+  try {
+    await transfer.save();
+
+    if (!transfer) {
+      throwError("Problems sending a transfer", 422);
+    }
+
+    res.status(201).json({ message: "Transfer sent" });
+  } catch (err) {
+    passError(err, next);
+  }
+};
