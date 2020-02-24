925b65b0bb500f11d98ae4279062504bd7037ff9
ionpascaru <iionpascaru@gmail.com>
2020-02-24 15:01:52 +0100

    cards calls

--- front/src/api/auth.js
+++ front/src/api/auth.js
@@ -1,14 +1,15 @@
-
-import { callAPI } from './base';
+import { callAPI } from "./base";
 
 // Register user
-export const registerUser = data => callAPI(`/auth/register`, 'post', data);
+export const registerUser = data => callAPI(`/auth/register`, "post", data);
 
 // Login a user
-export const loginUser = data => callAPI(`/auth/login`, 'post', data);
+export const loginUser = data => callAPI(`/auth/login`, "post", data);
 
 // Remind user's password - send a confirmation link
-export const remindPassword = data => callAPI(`/auth/remind-password`, 'post', data);
+export const remindPassword = data =>
+  callAPI(`/auth/remind-password`, "post", data);
 
 // Send user a new password
-export const resetPassword = data => callAPI(`/auth/reset-password`, 'post', data);
+export const resetPassword = data =>
+  callAPI(`/auth/reset-password`, "post", data);
