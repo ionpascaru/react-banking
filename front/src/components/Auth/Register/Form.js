f1b3b6dfbbb8dd8c293430fbd9561b9f10793926
ionpascaru <iionpascaru@gmail.com>
2020-02-24 15:28:19 +0100

    register created

--- front/src/components/Auth/Register/Form.js
+++ front/src/components/Auth/Register/Form.js
@@ -1,20 +1,68 @@
 import React from "react";
-import SmallFormBox from "components/UI/FormBoxes/Small";
-import RegisterForm from "./Form";
-import Loader from "components/UI/Loader";
+import { Form, Field, withFormik } from "formik";
+import { Link } from "react-router-dom";
+import * as Yup from "yup";
+import SingleModuleButton from "components/UI/Buttons/SingleModuleButton";
 
-const RegisterBox = props => (
-  <SmallFormBox>
-    {props.error ? <p>{props.error.message}</p> : null}
-    {props.loading ? (
-      <Loader />
-    ) : (
-      <RegisterForm
-        history={props.history}
-        onRegisterSubmit={props.onRegisterSubmit}
-      />
-    )}
-  </SmallFormBox>
-);
+const InnerForm = props => {
+  const { errors, touched } = props;
 
-export default RegisterBox;
+  return (
+    <Form className="register-form">
+      <div>
+        <div className="form-group">
+          <Field
+            type="email"
+            className="form-control login-input"
+            name="email"
+            placeholder="Your email..."
+          />
+          {touched.email && errors.email && (
+            <p className="field-invalid">{errors.email}</p>
+          )}
+        </div>
+
+        <div className="form-group">
+          <Field
+            type="password"
+            className="form-control password-input"
+            name="password"
+            placeholder="Your password..."
+          />
+          {touched.password && errors.password && (
+            <p className="field-invalid">{errors.password}</p>
+          )}
+        </div>
+
+        <p>
+          <Link to="/login">Already have an account?</Link>
+        </p>
+      </div>
+
+      <SingleModuleButton text="Create your account" type="submit" />
+    </Form>
+  );
+};
+
+// Wrap our form with the using withFormik HoC
+const RegisterForm = withFormik({
+  // Transform outer props into form values
+  mapPropsToValues: props => ({ email: "", password: "" }),
+
+  // Add a custom validation 
+  validationSchema: Yup.object().shape({
+    email: Yup.string()
+      .required("Email is required")
+      .email("This is not a valid email"),
+    password: Yup.string()
+      .required("Password is required")
+      .min(6, "Your password has to be at least 6 characters")
+  }),
+
+  // Submission handler
+  handleSubmit: (values, { props, setStatus }) => {
+    props.onRegisterSubmit(values.email, values.password);
+  }
+})(InnerForm);
+
+export default RegisterForm;
