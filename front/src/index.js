52684ba0d5caba3942ef22a932e75e881cf735bf
ionpascaru <iionpascaru@gmail.com>
2020-02-25 12:40:13 +0100

    store

--- front/src/index.js
+++ front/src/index.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import App from "components/App";
+import * as serviceWorker from "./serviceWorker";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+import configureStore, { history } from "store";
+import { ConnectedRouter } from "connected-react-router";
+
+export const store = configureStore();
+
+// Render
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <ConnectedRouter history={history}>
+        <App />
+      </ConnectedRouter>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
+);
+
 serviceWorker.unregister();
